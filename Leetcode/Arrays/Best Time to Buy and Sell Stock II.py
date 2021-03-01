27f0778be7d836201946b0c5f64e156a286a5655
Rizwan Memon <rizwan2000.rm@gmail.com>
2021-03-01 17:22:44 +0530

    Remove Duplicate from sorted array, best time to buy stocks 2

---
+++ Leetcode/Arrays/Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,8 @@
+def maxProfit(prices):
+    profit = 0
+
+    for i in range(len(prices)-1):
+        if prices[i] < prices[i+1]:
+            profit += (prices[i+1] - prices[i])
+
+    return profit
---
+++ Leetcode/Arrays/remove-duplicates-sorted.py
@@ -0,0 +1,7 @@
+def removeDuplicates(nums):
+    i = 0
+    for j in range(1, len(nums)):
+        if nums[i] != nums[j]:
+            i += 1
+            nums[i] = nums[j]
+    return i + 1
