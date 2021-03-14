ab6abda128526e9948162307e3631340b6172439
Rizwan Memon <rizwan2000.rm@gmail.com>
2021-03-14 14:35:55 +0530

    Python Data types

---
+++ Python/datatypes.py
@@ -0,0 +1,42 @@
+# Numeric - int, float, complex
+# String - list of characters
+# List - can be changed or modified, variable length
+# Tuple - immutable nature, fixed length
+# Boolean
+# Set - Hash Set
+# Dictionary - Hash Maps
+
+
+# Numeric
+a = 5
+print("Type of a: ", type(a)) 
+  
+b = 5.0
+print("\nType of b: ", type(b)) 
+  
+c = 2 + 4j
+print("\nType of c: ", type(c)) 
+
+# String
+String1 = 'Welcome to the Geeks World'
+print("\nType of String1: ", type(String1))
+
+# List
+List = [['Geeks', 'For'], ['Geeks']] 
+print("\nType of List: ", type(List))
+
+# Tuple
+Tuple1 = ('Geeks', 'For') 
+print("\nType of Tuple1: ", type(Tuple1))
+
+# Boolean
+print("\nType of Boolean: ",type(True))
+
+# Set
+set1 = set("GeeksForGeeks") 
+print("\nType of set1: ", type(set1))
+# print(set1)  # {'s', 'r', 'G', 'o', 'e', 'F', 'k'}
+
+# Dictionary
+Dict = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+print("\nType of Dict: ",type(Dict))
