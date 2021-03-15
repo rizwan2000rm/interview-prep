f189ea6ceba2aa5f60f478ffbd04e0e4cf15e4e8
Rizwan Memon <rizwan2000.rm@gmail.com>
2021-03-15 13:42:36 +0530

    Stack in Python

---
+++ Python/DS/stack.py
@@ -0,0 +1,16 @@
+print("============ stack ============")
+print()
+
+stack = [3, 4, 5]
+print(stack)
+print()
+
+stack.append(6)
+stack.append(7)
+print(stack)
+print()
+
+stack.pop()
+stack.pop()
+print(stack)
+print()
