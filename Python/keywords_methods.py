cb80fa86b0379744f37671ba27338b6c25c844a7
Rizwan Memon <rizwan2000.rm@gmail.com>
2021-03-15 14:11:44 +0530

    List Comprehensions in Python

---
+++ Python/keywords_methods.py
@@ -0,0 +1,14 @@
+# len
+print("============ len() ============")
+print()
+list=[1,2,3,4,5]
+print(len(list))
+
+# del
+print("============ del x ============")
+print()
+a = [-1, 1, 66.25, 333, 333, 1234.5]
+del a[0]
+print(a)
+del a[2:4]
+print(a)
---
+++ Python/list-comprehension.py
@@ -0,0 +1,39 @@
+from math import pi
+# [(expression) (for loop)]
+
+print("============ List Comprehensions ============")
+print()
+
+# squares = []
+# for x in range(10):
+# squares.append(x**2)
+squares = [x**2 for x in range(10)]
+print(squares)
+print()
+
+# combs = []
+# for x in [1,2,3]:
+# for y in [3,1,4]:
+# if x != y:
+# combs.append((x, y))
+combinations = [(x, y) for x in [1,2,3] for y in [3,1,4] if x != y]
+print(combinations)
+print()
+
+roundedPI = [str(round(pi, i)) for i in range(1, 6)]
+print(roundedPI)
+print()
+
+print("============ Nested List Comprehensions ============")
+print()
+
+matrix = [
+[1, 2, 3, 4],
+[5, 6, 7, 8],
+[9, 10, 11, 12],
+]
+
+# The following list comprehension will transpose rows and columns:
+transposeMatrix = [[row[i] for row in matrix] for i in range(4)]
+print(transposeMatrix)
+print()
