9929b90479a663d94be9616fdf76e5f90e7251f9
Rizwan Memon <rizwan2000.rm@gmail.com>
2021-03-15 16:17:39 +0530

    Tuples in Python

---
+++ Python/DS/tuple.py
@@ -0,0 +1,16 @@
+# Tuples are immutable
+print("============ tuples ============")
+print()
+
+tuples = (12345, 54321, 'hello!')
+
+print(tuples)
+
+u = tuples, (1, 2, 3, 4, 5)
+print(u)
+
+# The statement t = 12345, 54321, 'hello!' is an example of tuple packing:
+# the values 12345, 54321 and 'hello!'
+# are packed together in a tuple. The reverse operation is also possible
+x, y, z = tuples
+print(x,y,z)
